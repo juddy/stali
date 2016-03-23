e7b3d4c1a972cdfd5dc2990560bb45bf4426ee0b
Anselm R Garbe <anselm@garbe.us>
2016-03-23 19:28:05 +0100

    several etc tweaks

--- etc/stali.mk
+++ etc/stali.mk
@@ -3,8 +3,12 @@ ROOT=..
 include $(ROOT)/config.mk
 
 ETC = fstab.def\
+	group.def\
 	hosts.conf.def\
-	rc.conf.def
+	passwd.def\
+	profile.def\
+	rc.conf.def\
+	shells.def
 
 include $(ROOT)/mk/etc.mk
 
