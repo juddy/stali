bb832dbcfd132931afe152feb6aec0baed737d9d
Anselm R Garbe <anselm@garbe.us>
2016-08-17 14:51:31 +0200

    introducing arm instead of arm64 for now (musl lacks arm64 support atm)

--- config.mk
+++ config.mk
@@ -2,23 +2,23 @@
 VERSION = 0.0
 
 # paths
-DESTDIR=$(HOME)/rootfs-arm64
+DESTDIR=$(HOME)/rootfs-arm
 PREFIX = /
 MANPREFIX = $(PREFIX)/share/man
 
 M4 = m4
-CC = $(ROOT)/../toolchain/arm64-linux-musl/bin/arm64-linux-musl-gcc
-CXX = $(ROOT)/../toolchain/arm64-linux-musl/bin/arm64-linux-musl-g++
-AS = $(ROOT)/../toolchain/arm64-linux-musl/bin/arm64-linux-musl-as
+CC = $(ROOT)/../toolchain/bin/arm-linux-musl-gcc
+CXX = $(ROOT)/../toolchain/bin/arm-linux-musl-g++
+AS = $(ROOT)/../toolchain/bin/arm-linux-musl-as
 LD = $(CC)
 
 YACC = $(ROOT)/bin/hbase/yacc/yacc
-AR = $(ROOT)/../toolchain/arm64-linux-musl/bin/arm64-linux-musl-ar
-RANLIB = $(ROOT)/../toolchain/arm64-linux-musl/bin/arm64-linux-musl-ranlib
+AR = $(ROOT)/../toolchain/bin/arm-linux-musl-ar
+RANLIB = $(ROOT)/../toolchain/bin/arm-linux-musl-ranlib
 
 CPPFLAGS = -D_POSIX_SOURCE -D__stali__
-CFLAGS   = -I$(ROOT)/../toolchain/arm64-linux-musl/arm64-linux-musl/include
-CXXFLAGS   = -I$(ROOT)/../toolchain/arm64-linux-musl/arm64-linux-musl/include
+CFLAGS   = -I$(ROOT)/../toolchain/arm-linux-musl/include
+CXXFLAGS   = -I$(ROOT)/../toolchain/arm-linux-musl/include
 #-std=c99 -Wall -pedantic
 #LDFLAGS  = -s -static
 LDFLAGS  = -static
