92e3a49e61f2b482489168846a765f9cebb75b49
Anselm R Garbe <anselm@garbe.us>
2016-08-22 09:04:53 +0200

    use kernel7.img instead of kernel.img (outdated)

--- sys/stali.mk
+++ sys/stali.mk
@@ -1,23 +1,26 @@
 ROOT=..
+KERNEL=kernel7
 
 include $(ROOT)/config.mk
 
 all:
-	@$(MAKE) ARCH=arm || exit
+	@$(MAKE) -j7 KERNEL=$(KERNEL) ARCH=arm zImage dtbs || exit
 	@echo done
 
 install:
 	@mkdir -p $(DESTDIR)/boot/overlays
 	@cp arch/arm/boot/dts/*.dtb $(DESTDIR)/boot/
-	@cp arch/arm/boot/dts/overlays/*.dtbo $(DESTDIR)/boot/overlays/
-	@scripts/mkknlimg arch/arm/boot/zImage $(DESTDIR)/boot/kernel.img
+	@cp arch/arm/boot/dts/overlays/*.dtb* $(DESTDIR)/boot/overlays/
+	@cp arch/arm/boot/dts/overlays/README $(DESTDIR)/boot/overlays/
+	@scripts/mkknlimg arch/arm/boot/zImage $(DESTDIR)/boot/$(KERNEL).img
 	@cp .config $(DESTDIR)/boot/config
 	@echo installed
 
 uninstall:
 	@rm -f $(DESTDIR)/boot/config
 	@rm -f $(DESTDIR)/boot/kernel.img
-	@rm -f $(DESTDIR)/boot/overlays/*.dtbo
+	@rm -f $(DESTDIR)/boot/overlays/*.dtb*
+	@rm -f $(DESTDIR)/boot/overlays/README
 	@rm -f $(DESTDIR)/boot/*.dtb
 	@echo uninstalled
 
